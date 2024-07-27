8c9b41f55d382b8833b229c50559db20f5be3e71
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-27 18:27:51 +0200

    Update arsenal4.lua

--- arsenal4.lua
+++ arsenal4.lua
@@ -4,7 +4,7 @@ local Window = Library.CreateLib("Dawido2137POK", "DarkTheme")
 
 --ARSENAL
 local arse = Window:NewTab("Arsenal")
-local arsenalSection = arse:NewSection("All")
+local arsenalSection = arse:NewSection("Scripts")
 
 arsenalSection:NewButton("OwlHub", "Execute the owlhub script", function()
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"),true))()
