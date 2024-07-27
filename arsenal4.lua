4eaae875343d9d164b54f29e1a20d2dce38fd84d
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-27 16:57:31 +0200

    Update arsenal4.lua

--- arsenal4.lua
+++ arsenal4.lua
@@ -1,2 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Dawido2137POK", "DarkTheme")
+
+
+--ARSENAL
+local arsenal = Window:NewTab("Arsenal")
+local arsenalSection = Tab:NewSection("Owlhub")
