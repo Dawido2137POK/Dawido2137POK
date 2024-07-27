2a29e979c311148813f95f3035c2af6e62f43e7c
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-27 17:58:49 +0200

    Update arsenal4.lua

--- arsenal4.lua
+++ arsenal4.lua
@@ -10,10 +10,29 @@ arsenalSection:NewButton("OwlHub", "Execute the owlhub script", function()
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"),true))()
 end)
 
-arsenalSection:NewToggle("Extra Speed", "Give a SPEED", function(state)
-    if state then
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
-    else
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-    end
+
+
+--UNIVERSAL
+local universals = Window:NewTab("Universal")
+local universalSection = universals:NewSection("Player")
+
+
+universalSection:NewSlider("Speed", "Select Your Speed", 100, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+universalSection:NewSlider("Jump Power", "Select Your Jump Power", 150, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+
+local universalskSection = universals:NewSection("Scripts")
+
+universalskSection:NewButton("Infinite Yield", "Execute the Infinite Yield script", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+
+universalskSection:NewButton("Dex Explorer", "Execute the Dex Explorer script", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Dawido2137POK/Dawido2137POK/main/dex.lua'))()
 end)
