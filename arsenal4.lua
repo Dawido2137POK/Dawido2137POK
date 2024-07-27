c6d61b890431634d3531b05afd8191879b469771
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-27 17:24:53 +0200

    Update arsenal4.lua

--- arsenal4.lua
+++ arsenal4.lua
@@ -4,13 +4,13 @@ local Window = Library.CreateLib("Dawido2137POK", "DarkTheme")
 
 --ARSENAL
 local arse = Window:NewTab("Arsenal")
-local owlSection = arse:NewSection("All")
+local allSection = arse:NewSection("All")
 
-owlSection:NewButton("OwlHub", "Execute the owlhub script", function()
+allSection:NewButton("OwlHub", "Execute the owlhub script", function()
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"),true))()
 end)
 
-owlSection:NewToggle("ToggleText", "ToggleInfo", function(state)
+allSection:NewToggle("ToggleText", "ToggleInfo", function(state)
     if state then
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 50
     else
