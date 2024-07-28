dfb9b3fceb0cc012dbd2fe8a073ddb49493064fc
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-28 12:50:37 +0200

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -1,10 +1,18 @@
+ 
+--HITBOX EXPANDER
+_G.HeadSize = 25 _G.Disabled = true game:GetService('RunService').RenderStepped:connect(function() if _G.Disabled then for i,v in next, game:GetService('Players'):GetPlayers() do if v.Name ~= game:GetService('Players').LocalPlayer.Name then pcall(function() v.Character.Head.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize) v.Character.Head.Transparency = 0.7 v.Character.Head.BrickColor = BrickColor.new("Red") v.Character.Head.Material = "Neon" v.Character.Head.CanCollide = false v.Character.Head.Massless = true end) end end end end)
 
+while wait() do
+	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
+	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
+end
 
 game.StarterGui:SetCore("SendNotification", {
     Title = "INJECTED!";
     Text = ""; 
     Duration = 30;
 })
+
 local replicationstorage = game.ReplicatedStorage
  
 for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
@@ -27,29 +35,8 @@ for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
        v.Value = 20
    end
 end
- 
- 
 
-local gui = Instance.new("BillboardGui");
-gui.Name = "";
-gui.AlwaysOnTop = true;
-gui.LightInfluence = 0;
-gui.Size = UDim2.new(1.75, 0, 1.75, 0);
-local frame = Instance.new("Frame", gui);
-frame.BackgroundColor3 = Color3.fromRGB(170, 0, 0);
-frame.Size = UDim2.new(1, 0, 1, 0);
-frame.BorderSizePixel = 4;
-frame.BorderColor3 = Color3.fromRGB(0, 0, 0);
-local gi = gui:Clone();
-local body = frame:Clone();
-body.Parent = gi;
-body.BackgroundColor3 = Color3.fromRGB(0, 170, 170);
- 
-for _, v in pairs(game:GetService("Players"):GetPlayers()) do
-    if v.Name ~= game:GetService("Players").LocalPlayer.Name and v.Character and v.Character:FindFirstChild("Head") then
-        gui:Clone().Parent = v.Character.Head;
-    end
+while wait() do
+	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
+	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
 end
- 
---HITBOX EXPANDER
-_G.HeadSize = 25 _G.Disabled = true game:GetService('RunService').RenderStepped:connect(function() if _G.Disabled then for i,v in next, game:GetService('Players'):GetPlayers() do if v.Name ~= game:GetService('Players').LocalPlayer.Name then pcall(function() v.Character.Head.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize) v.Character.Head.Transparency = 0.7 v.Character.Head.BrickColor = BrickColor.new("Red") v.Character.Head.Material = "Neon" v.Character.Head.CanCollide = false v.Character.Head.Massless = true end) end end end end)
