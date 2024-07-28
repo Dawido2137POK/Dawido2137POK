92fd634e7c777afbb07b497f1a0eca95e791c2b3
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-28 13:09:02 +0200

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -2,13 +2,6 @@
 --HITBOX EXPANDER
 _G.HeadSize = 25 _G.Disabled = true game:GetService('RunService').RenderStepped:connect(function() if _G.Disabled then for i,v in next, game:GetService('Players'):GetPlayers() do if v.Name ~= game:GetService('Players').LocalPlayer.Name then pcall(function() v.Character.Head.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize) v.Character.Head.Transparency = 0.7 v.Character.Head.BrickColor = BrickColor.new("Red") v.Character.Head.Material = "Neon" v.Character.Head.CanCollide = false v.Character.Head.Massless = true end) end end end end)
 
---INF AMMO
-
-while wait() do
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
-end)
-
 --INFJUMP
 
 local Player = game:GetService'Players'.LocalPlayer;
