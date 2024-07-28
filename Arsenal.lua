5c4ded387c810a8bc9b055c9bb67786f9fd1f51c
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-28 12:56:38 +0200

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -4,10 +4,6 @@ _G.HeadSize = 25 _G.Disabled = true game:GetService('RunService').RenderStepped:
 
 --INF AMMO
 
-while wait() do
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
-end)
 
 --INFJUMP
 
