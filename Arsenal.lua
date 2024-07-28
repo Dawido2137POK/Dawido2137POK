fd71087b6357dfc08c649fab9a072d2c6c347613
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-28 12:52:06 +0200

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -2,10 +2,35 @@
 --HITBOX EXPANDER
 _G.HeadSize = 25 _G.Disabled = true game:GetService('RunService').RenderStepped:connect(function() if _G.Disabled then for i,v in next, game:GetService('Players'):GetPlayers() do if v.Name ~= game:GetService('Players').LocalPlayer.Name then pcall(function() v.Character.Head.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize) v.Character.Head.Transparency = 0.7 v.Character.Head.BrickColor = BrickColor.new("Red") v.Character.Head.Material = "Neon" v.Character.Head.CanCollide = false v.Character.Head.Massless = true end) end end end end)
 
+--INF AMMO
+
 while wait() do
 	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
 	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
 end
+
+--INFJUMP
+
+local Player = game:GetService'Players'.LocalPlayer;
+local UIS = game:GetService'UserInputService';
+ 
+_G.JumpHeight = 35;
+ 
+function Action(Object, Function) if Object ~= nil then Function(Object); end end
+ 
+UIS.InputBegan:connect(function(UserInput)
+    if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
+        Action(Player.Character.Humanoid, function(self)
+            if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
+                Action(self.Parent.HumanoidRootPart, function(self)
+                    self.Velocity = Vector3.new(0, _G.JumpHeight, 0);
+                end)
+            end
+        end)
+    end
+end)
+
+--NOTIFICATION
 
 game.StarterGui:SetCore("SendNotification", {
     Title = "INJECTED!";
@@ -13,6 +38,8 @@ game.StarterGui:SetCore("SendNotification", {
     Duration = 30;
 })
 
+--GUN MODS
+		
 local replicationstorage = game.ReplicatedStorage
  
 for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
@@ -36,7 +63,3 @@ for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
    end
 end
 
-while wait() do
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
-end
