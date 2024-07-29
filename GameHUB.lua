3fa8318e7aac1dad5d73c197fda96314dbed0182
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-07-29 10:09:15 +0200

    Update GameHUB.lua

--- GameHUB.lua
+++ GameHUB.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Game HUB", "Sentinel")
+local Window = Library.CreateLib("Game HUB", "GrapeTheme")
 
 
 --ARSENAL
