cbb7a99f4eb028adde83d0c37aabe3913bd37e8a
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-08-04 10:55:22 +0200

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -84,9 +84,6 @@ for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
    if v.Name == "FireRate" then
        v.Value = 0.05
    end
-   if v.Name == "Crit" then
-       v.Value = 20
-   end
 end
 
 --ESP
