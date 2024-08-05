21ff90a60d3468b17b8efdbffa163baeaec07705
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2024-08-05 21:24:47 +0200

    Update ArsenalMyLite.lua

--- ArsenalMyLite.lua
+++ ArsenalMyLite.lua
@@ -85,6 +85,11 @@ end)
            v.Value = 0.0700
        end
     end
+
+
+--Rainbow Gun
+
+    local c = 1 function zigzag(X)  return math.acos(math.cos(X * math.pi)) / math.pi end game:GetService("RunService").RenderStepped:Connect(function()  if game.Workspace.Camera:FindFirstChild('Arms') then   for i,v in pairs(game.Workspace.Camera.Arms:GetDescendants()) do    if v.ClassName == 'MeshPart' then      v.Color = Color3.fromHSV(zigzag(c),1,1)     c = c + .0001    end   end  end end)
     
 
 --ESP
