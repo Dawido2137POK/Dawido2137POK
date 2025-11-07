3f7aaeb66f9a0f5baf7acb5f333ec34b1928acb5
Dawido2137POK <131606434+Dawido2137POK@users.noreply.github.com>
2025-11-07 22:24:28 +0100

    Update OrionHubGameTest.lua

--- OrionHubGameTest.lua
+++ OrionHubGameTest.lua
@@ -1,6 +1,6 @@
 --KEY SYSTEM 1/2
 
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local WindowKey = OrionLib:MakeWindow({Name = "GameHub - Key System", HidePremium = false, IntroEnabled = false, IntroText = "Game Hub", SaveConfig = true, ConfigFolder = "GameHub-Orion"})
 
 --Values
